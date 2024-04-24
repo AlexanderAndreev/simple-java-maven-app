9562586ee3b382cdb157c3317044e7b4e7c9ed32
Alexandr Andreev <alexandr_andreev@epam.com>
2024-04-24 20:42:29 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,16 +8,24 @@ set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
+echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn help:evaluate -B -Dexpression=project.name | grep "^[^\[]"`
+NAME=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.name}' \
+    --non-recursive \
+    exec:exec)
 set +x
 
-echo 'The following complex command behaves similarly to the previous one but'
+echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn help:evaluate -B -Dexpression=project.version | grep "^[^\[]"`
+VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
